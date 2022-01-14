8e95ee11f045d4bca080e211faf909326f0cdad6
W Rivera <wrivera@Ws-iMac.fios-router.home>
2022-01-13 19:25:41 -0500

    first

---
+++ src/pages/blog.js
@@ -0,0 +1,32 @@
+import usePageMetadata from 'hooks/use-page-metadata';
+
+import { getPaginatedPosts } from 'lib/posts';
+
+import TemplateArchive from 'templates/archive';
+
+export default function Posts({ posts, pagination }) {
+  const title = 'Blogz Posters';
+  const slug = 'blog';
+
+  const { metadata } = usePageMetadata({
+    metadata: {
+      title,
+      description: false,
+    },
+  });
+
+  return <TemplateArchive title={title} posts={posts} slug={slug} pagination={pagination} metadata={metadata} />;
+}
+
+export async function getStaticProps() {
+  const { posts, pagination } = await getPaginatedPosts();
+  return {
+    props: {
+      posts,
+      pagination: {
+        ...pagination,
+        basePath: '/blog',
+      },
+    },
+  };
+}
